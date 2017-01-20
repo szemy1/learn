4baada1c836fb50f3f4b40b11e329cb94c86129c
szemy <szemy1@gmail.com>
2017-01-21 00:45:37 +0100

    IMrt transliteration

--- Transform_Translate.py
+++ Transform_Translate.py
@@ -3,10 +3,11 @@ from transliterate import translit
 a = "KOLA"
 b = a[0]+a[1].lower()+a[2].lower()+a[3].lower()
 c = a.lower()
+cirill_a =(translit(a, 'ru'))
 cirill_b =(translit(b, 'ru'))
 cirill_c =(translit(c, 'ru'))
 
-
+print cirill_a
 print cirill_c
 print cirill_b
 print a
