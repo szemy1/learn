1745b2cf9a3cc2e8f02b5c7411ab9ba30d595b3c
szemy <szemy1@gmail.com>
2017-01-21 00:41:17 +0100

    IMrt transliteration

--- Transform_Translate.py
+++ Transform_Translate.py
@@ -2,5 +2,13 @@
 from transliterate import translit, get_available_language_codes
 a = "KOLA"
 b = a[0]+a[1].lower()+a[2].lower()+a[3].lower()
-print(translit(b, 'ru'))
+c = a.lower()
+cirill_b =(translit(b, 'ru'))
+cirill_c =(translit(c, 'ru'))
 
+
+print cirill_c
+print cirill_b
+print a
+print b
+print c
