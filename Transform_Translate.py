2342c92a05f5d11f34625824cf34c990698ef396
szemy <szemy1@gmail.com>
2017-01-21 10:17:45 +0100

    IMrt transliteration

--- Transform_Translate.py
+++ Transform_Translate.py
@@ -1,21 +1,36 @@
-#!/bin/python
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import csv
+import codecs
+import os
+import sys
 from transliterate import translit
+
+
+def last():
+    return self.dict[ self.last ]
+
+a= []
 with open('eggs.csv', 'rb') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
     for row in spamreader:
-       a =  ', '.join(row)
+        a.append(', '.join(row))
+        #lista = list(a.items())
 
-#a = "KOLA"
-b = a.title()
-c = a.upper()
-cirill_a =(translit(a, 'ru'))
-cirill_b =(translit(b, 'ru'))
-cirill_c =(translit(c, 'ru'))
-
-print cirill_a
-print cirill_c
-print cirill_b
-print a
-print b
-print c
+        #b = a.title()
+        #c = a.upper()
+        cirill_a =(translit(a, 'ru'))
+        #cirill_a_list=[cirill_a]
+        #cirill_a_list_enc = cirill_a_list.encode('utf8')
+        #cirill_b =(translit(b, 'ru'))
+        #cirill_c =(translit(c, 'ru'))
+        print a
+        print cirill_a
+        cirill_a.encode('utf-8').split()
+        c=str(a)
+        #a.encode('utf-8').split()
+        #print cirill_b
+        codecs.open("generalt.csv", 'a', "utf-8").close()
+        generaltfile= codecs.open ("generalt.csv",'a', "utf-8")
+        generaltfile.write(cirill_a+'\n'+c)
+        generaltfile.close()
