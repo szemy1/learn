8000970da60cbfa42acfdc739fc840b07f185fe7
szemy <szemy1@gmail.com>
2017-01-23 20:26:18 +0100

    Szemy

--- teszt22.py
+++ teszt22.py
@@ -3,19 +3,40 @@
 
 import csv
 import codecs
+from Tkinter import Tk
+from tkFileDialog import askopenfilename
 from transliterate import translit
 
 a = "търсене"
+Tk().withdraw()
+csvfajl = askopenfilename()
 # def melo(self):
 #     b= True
 #     (translit(self.decode(encoding='utf-8', errors='strict'), 'bg', reversed=b))
 
-latinkicsi=(translit(a.decode(encoding='utf-8', errors='strict'), 'bg', reversed=True)).lower()
-latinnagy=(translit(a.decode(encoding='utf-8', errors='strict'), 'bg', reversed=True)).upper()
-latincapital=(translit(a.decode(encoding='utf-8', errors='strict'), 'bg', reversed=True)).title()
-cirillkicsi=(translit(a.decode(encoding='utf-8', errors='strict'), 'bg', reversed=False)).upper()
-cirillnagy= (translit(a.decode(encoding='utf-8', errors='strict'), 'bg', reversed=False)).lower()
-cirillcapital= (translit(a.decode(encoding='utf-8', errors='strict'), 'bg', reversed=False)).title()
+olvasas = csv.reader(open(csvfajl,"rb"))
+for row in olvasas:
+    print row
+    forditas = raw_input("Forditas:")
+    str(forditas)
+    latinkicsi = (translit(forditas.decode(encoding='utf-8', errors='strict'), 'bg', reversed=True)).lower()
+    latinnagy = (translit(forditas.decode(encoding='utf-8', errors='strict'), 'bg', reversed=True)).upper()
+    latincapital = (translit(forditas.decode(encoding='utf-8', errors='strict'), 'bg', reversed=True)).title()
+    cirillkicsi = (translit(forditas.decode(encoding='utf-8', errors='strict'), 'bg', reversed=False)).upper()
+    cirillnagy = (translit(forditas.decode(encoding='utf-8', errors='strict'), 'bg', reversed=False)).lower()
+    cirillcapital = (translit(forditas.decode(encoding='utf-8', errors='strict'), 'bg', reversed=False)).title()
+    codecs.open("generalt.csv", 'a', "utf-8").close()
+    generaltfile = codecs.open("generalt.csv", 'a', "utf-8")
+    generaltfile.write(latinkicsi+";"+latinnagy+";"+latincapital+";"+cirillkicsi+";"+cirillnagy+";"+cirillcapital)
+    print "Beirva: "+latinkicsi + ";" + latinnagy + ";" + latincapital + ";" + cirillkicsi + ";" + cirillnagy + ";" + cirillcapital
+    generaltfile.close()
+
+# latinkicsi=(translit(a.decode(encoding='utf-8', errors='strict'), 'bg', reversed=True)).lower()
+# latinnagy=(translit(a.decode(encoding='utf-8', errors='strict'), 'bg', reversed=True)).upper()
+# latincapital=(translit(a.decode(encoding='utf-8', errors='strict'), 'bg', reversed=True)).title()
+# cirillkicsi=(translit(a.decode(encoding='utf-8', errors='strict'), 'bg', reversed=False)).upper()
+# cirillnagy= (translit(a.decode(encoding='utf-8', errors='strict'), 'bg', reversed=False)).lower()
+# cirillcapital= (translit(a.decode(encoding='utf-8', errors='strict'), 'bg', reversed=False)).title()
 # print(translit(a.decode(encoding='utf-8', errors='strict'), 'bg', reversed=True)).lower()
 # print(translit(a.decode(encoding='utf-8', errors='strict'), 'bg', reversed=True)).upper()
 # print(translit(a.decode(encoding='utf-8', errors='strict'), 'bg', reversed=True)).title()
