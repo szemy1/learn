5cd9149692c8fe71a77912e73befabd7fed853b9
K3chk3 <gergo.grigorov@gmail.com>
2017-01-23 19:56:52 +0100

    elso

--- teszt22.py
+++ teszt22.py
@@ -5,17 +5,19 @@ import csv
 import codecs
 from transliterate import translit
 
-a = "търсене"
+a = "трудово право"
+
 # def melo(self):
 #     b= True
 #     (translit(self.decode(encoding='utf-8', errors='strict'), 'bg', reversed=b))
+# @Szemy: átírtam az upper/lower/title részt, mert elcsúsztak a magyarral szemben, most jó azt hiszem. :D
 
+cirillkicsi=(translit(a.decode(encoding='utf-8', errors='strict'), 'bg', reversed=False)).lower()
+cirillnagy= (translit(a.decode(encoding='utf-8', errors='strict'), 'bg', reversed=False)).upper()
+cirillcapital= (translit(a.decode(encoding='utf-8', errors='strict'), 'bg', reversed=False)).title()
 latinkicsi=(translit(a.decode(encoding='utf-8', errors='strict'), 'bg', reversed=True)).lower()
 latinnagy=(translit(a.decode(encoding='utf-8', errors='strict'), 'bg', reversed=True)).upper()
 latincapital=(translit(a.decode(encoding='utf-8', errors='strict'), 'bg', reversed=True)).title()
-cirillkicsi=(translit(a.decode(encoding='utf-8', errors='strict'), 'bg', reversed=False)).upper()
-cirillnagy= (translit(a.decode(encoding='utf-8', errors='strict'), 'bg', reversed=False)).lower()
-cirillcapital= (translit(a.decode(encoding='utf-8', errors='strict'), 'bg', reversed=False)).title()
 # print(translit(a.decode(encoding='utf-8', errors='strict'), 'bg', reversed=True)).lower()
 # print(translit(a.decode(encoding='utf-8', errors='strict'), 'bg', reversed=True)).upper()
 # print(translit(a.decode(encoding='utf-8', errors='strict'), 'bg', reversed=True)).title()
@@ -23,4 +25,5 @@ cirillcapital= (translit(a.decode(encoding='utf-8', errors='strict'), 'bg', reve
 # print(translit(a.decode(encoding='utf-8', errors='strict'), 'bg', reversed=False)).title()
 # print(translit(a.decode(encoding='utf-8', errors='strict'), 'bg', reversed=False)).lower()
 
-print latinkicsi+";"+latinnagy+";"+latincapital+";"+cirillkicsi+";"+cirillnagy+";"+cirillcapital
+# print latinkicsi+";"+latinnagy+";"+latincapital+";"+cirillkicsi+";"+cirillnagy+";"+cirillcapital
+print cirillkicsi+";"+cirillnagy+";"+cirillcapital+";"+latinkicsi+";"+latinnagy+";"+latincapital
