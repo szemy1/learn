27bb681b173a84a19e613ee6aca23bf7020136ff
szemy <szemy1@gmail.com>
2017-01-24 19:32:15 +0100

    Version 0.1 config parameter language and separator

--- Bilanguage-Transform.py
+++ Bilanguage-Transform.py
@@ -6,6 +6,9 @@ from Tkinter import Tk
 from tkFileDialog import askopenfilename
 from transliterate import translit
 
+
+language = raw_input("Nyelv:")
+szeparator = raw_input(u"alkalmazott szeparátor:")
 Tk().withdraw()
 csvfajl = askopenfilename()
 olvasas = csv.reader(open(csvfajl,"rb"))
@@ -14,14 +17,14 @@ for row in olvasas:
     print forditando.decode(encoding='iso-8859-1')
     forditas = raw_input("Forditas:")
     str(forditas)
-    latinkicsi = (translit(forditas.decode(encoding='utf-8', errors='strict'), 'bg', reversed=True)).lower()
-    latinnagy = (translit(forditas.decode(encoding='utf-8', errors='strict'), 'bg', reversed=True)).upper()
-    latincapital = (translit(forditas.decode(encoding='utf-8', errors='strict'), 'bg', reversed=True)).title()
-    cirillnagy = (translit(forditas.decode(encoding='utf-8', errors='strict'), 'bg', reversed=False)).upper()
-    cirillkicsi = (translit(forditas.decode(encoding='utf-8', errors='strict'), 'bg', reversed=False)).lower()
-    cirillcapital = (translit(forditas.decode(encoding='utf-8', errors='strict'), 'bg', reversed=False)).title()
+    latinkicsi = (translit(forditas.decode(encoding='utf-8', errors='strict'), language, reversed=True)).lower()
+    latinnagy = (translit(forditas.decode(encoding='utf-8', errors='strict'), language, reversed=True)).upper()
+    latincapital = (translit(forditas.decode(encoding='utf-8', errors='strict'), language, reversed=True)).title()
+    cirillnagy = (translit(forditas.decode(encoding='utf-8', errors='strict'), language, reversed=False)).upper()
+    cirillkicsi = (translit(forditas.decode(encoding='utf-8', errors='strict'), language, reversed=False)).lower()
+    cirillcapital = (translit(forditas.decode(encoding='utf-8', errors='strict'), language, reversed=False)).title()
     codecs.open("generalt.csv", 'a', "UTF-8").close()
     generaltfile = codecs.open("generalt.csv", 'a', "UTF-8")
     generaltfile.write(cirillkicsi+"|"+latinkicsi+";"+latinnagy+";"+latincapital+";"+cirillkicsi+";"+cirillnagy+";"+cirillcapital+"\n")
-    print "Beirva: "+cirillkicsi+"|"+latinkicsi + ";" + latinnagy + ";" + latincapital + ";" + cirillkicsi + ";" + cirillnagy + ";" + cirillcapital
+    print "Beirva: "+cirillkicsi+szeparator+latinkicsi + ";" + latinnagy + ";" + latincapital + ";" + cirillkicsi + ";" + cirillnagy + ";" + cirillcapital
     generaltfile.close()
